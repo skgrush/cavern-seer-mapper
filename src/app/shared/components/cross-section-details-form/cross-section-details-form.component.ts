59f7b43782a0c61956b7e9b5f0450742ef68c423
Samuel K. Grush <Samuel@Grush.org>
2023-12-29 21:27:20 -0600

    Buttons for moving the cross-section forwards and backwards

--- src/app/shared/components/cross-section-details-form/cross-section-details-form.component.ts
+++ src/app/shared/components/cross-section-details-form/cross-section-details-form.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CrossSectionAnnotation } from '../../models/annotations/cross-section.annotation';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CrossSectionAnnotation, degreesPerRadian } from '../../models/annotations/cross-section.annotation';
+import { ISimpleVector3 } from '../../models/simple-types';
 
 
 export type CrossSectionDetailsForm = FormGroup<{
@@ -25,7 +29,7 @@ export type CrossSectionDetailsForm = FormGroup<{
   templateUrl: './cross-section-details-form.component.html',
   styleUrl: './cross-section-details-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatTooltipModule],
 })
 export class CrossSectionDetailsFormComponent {
 
@@ -37,4 +41,19 @@ export class CrossSectionDetailsFormComponent {
 
   @Output()
   readonly formSubmitted = new EventEmitter<void>();
+
+  forward(meters: number) {
+    const { position, rotationDegrees } = this.formGroup.getRawValue();
+
+    const deltaX = Math.sin(rotationDegrees / degreesPerRadian) * meters;
+    const deltaZ = -Math.cos(rotationDegrees / degreesPerRadian) * meters;
+
+    const newVec: ISimpleVector3 = {
+      x: position.x + deltaX,
+      y: position.y,
+      z: position.z + deltaZ,
+    };
+
+    this.formGroup.controls.position.setValue(newVec);
+  }
 }
