b99078524c6861d57a6344ff9aa3146c28bf395e
Samuel K. Grush <Samuel@Grush.org>
2023-12-30 11:10:29 -0600

    use caching when loading from URL

--- src/app/pages/file-url-loader/file-url-loader.component.ts
+++ src/app/pages/file-url-loader/file-url-loader.component.ts
@@ -43,6 +43,8 @@ export class FileUrlLoaderComponent {
         }
         return { author, fileId, isAsset };
       }),
+      // on initial load of the page, query params are sometimes missing.
+      // we want to get the first non-nullish params
       ignoreNullish(),
       first(),
       switchMap(({ author, fileId, isAsset }) => {
@@ -54,6 +56,7 @@ export class FileUrlLoaderComponent {
 
         return fromFetch(new Request(url, {
           mode: 'same-origin',
+          cache: 'default',
         })).pipe(
           switchMap(resp => resp.blob()),
           switchMap(blob => {
