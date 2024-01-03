b74113e16ace6d2f81d9227b721f2294e64f2353
Samuel K. Grush <Samuel@Grush.org>
2024-01-03 12:06:17 -0600

    write version to ngsw-config.json appData.version

--- scripts/cicd/write-version-to-file-and-github-output.js
+++ scripts/cicd/write-version-to-file-and-github-output.js
@@ -1,6 +1,6 @@
 const { join } = require('node:path')
 const { getVersion } = require('../get-version')
-const { appendFile, writeFile } = require('node:fs/promises')
+const { appendFile, writeFile, readFile } = require('node:fs/promises')
 
 if (require.main === module) {
   void main()
@@ -39,15 +39,21 @@ async function main() {
     throw new Error('No env var GITHUB_OUTPUT')
   }
 
+  // write to GITHUB_OUTPUT
   await appendFile(githubOutput, `VERSION="${version}"\n`)
 
+  // write to version.json (used for MAPPER_VERSION token)
   const versionJsonPath = join(__dirname, '../../src/version.json')
-
   await writeFile(versionJsonPath, JSON.stringify(versionObject, undefined, 2))
 
-  const ngswConfigPath = '../../ngsw-config.json'
-  const ngswConfigJson = require(ngswConfigPath)
+  // write into ngsw-config.json
+  const ngswConfigPath = join(__dirname, '../../ngsw-config.json')
+  const ngswConfigJson = JSON.parse(await readFile(ngswConfigPath, { encoding: 'utf8' }))
   ngswConfigJson.appData.version = versionObject
 
-  await writeFile(ngswConfigPath, JSON.stringify(ngswConfigJson, undefined, 2))
+  await writeFile(ngswConfigPath, JSON.stringify(ngswConfigJson, undefined, 2), {
+    encoding: 'utf8',
+  })
+
+  console.info(JSON.stringify(ngswConfigJson, undefined, 2))
 }
