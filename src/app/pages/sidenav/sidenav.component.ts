972019b1985ad4977fca955b3f91e33e2101fe94
Samuel K. Grush <Samuel@Grush.org>
2024-01-09 17:22:41 -0600

    show version and github link in sidebar footer

--- src/app/pages/sidenav/sidenav.component.ts
+++ src/app/pages/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { DialogOpenerService } from '../../shared/services/dialog-opener.service';
@@ -8,14 +8,14 @@ import { MAPPER_VERSION } from '../../shared/tokens/version.token';
 @Component({
   selector: 'mapper-sidenav',
   standalone: true,
-  imports: [MatListModule, AsyncPipe],
+  imports: [MatListModule, AsyncPipe, NgIf],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidenavComponent {
 
-  readonly mapperVersion = inject(MAPPER_VERSION).version;
+  readonly mapperVersion = inject(MAPPER_VERSION);
   readonly dialogOpener = inject(DialogOpenerService);
   readonly #serviceWorker = inject(ServiceWorkerService);
 
