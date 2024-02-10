3d35d0269e638eac89032c821786213e73107b6a
Samuel K. Grush <Samuel@Grush.org>
2024-02-10 11:40:36 -0600

    Add MeshStandardMaterialService

--- src/app/shared/services/3d-managers/mesh-normal-material.service.ts
+++ src/app/shared/services/3d-managers/mesh-normal-material.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Color, MeshNormalMaterial } from 'three';
+import { MeshNormalMaterial } from 'three';
 import { BaseMaterialService } from './base-material.service';
 
 @Injectable()
 export class MeshNormalMaterialService extends BaseMaterialService<MeshNormalMaterial> {
-  override material: MeshNormalMaterial;
-
-
-  constructor() {
-    super();
-    this.material = new MeshNormalMaterial();
-    (this.material as any).color = new Color(0, 0, 0);
-  }
+  override readonly material = new MeshNormalMaterial();
 }
---
+++ src/app/shared/services/3d-managers/mesh-standard-material.service.ts
@@ -0,0 +1,13 @@
+import { BaseMaterialService } from './base-material.service';
+import { MeshStandardMaterial } from 'three';
+
+/**
+ * Requires a directional light.
+ */
+export class MeshStandardMaterialService extends BaseMaterialService<MeshStandardMaterial> {
+  override readonly material = new MeshStandardMaterial({
+    color: 0xCCCCCC,
+    metalness: 0.2,
+    flatShading: true,
+  });
+}
