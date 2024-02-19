853e4d8e452ff5c6613d40b7d85c684290d88355
Samuel K. Grush <Samuel@Grush.org>
2024-02-19 08:34:29 -0600

    Topographic material

--- src/app/shared/services/materials/index.ts
+++ src/app/shared/services/materials/index.ts
@@ -4,6 +4,7 @@ import { MeshNormalMaterialService } from './mesh-normal-material.service';
 import { MeshStandardMaterialService } from './mesh-standard-material.service';
 import { MaterialManagerService } from './material-manager.service';
 import { ElevationMaterialService } from './elevation-material.service';
+import { TopographicMaterialService } from './topographic-material.service';
 
 export function materialsProviders() {
   return makeEnvironmentProviders([
@@ -11,8 +12,10 @@ export function materialsProviders() {
     MeshNormalMaterialService,
     MeshStandardMaterialService,
     ElevationMaterialService,
+    TopographicMaterialService,
     { provide: MATERIAL_SERVICES, useExisting: MeshNormalMaterialService, multi: true },
     { provide: MATERIAL_SERVICES, useExisting: MeshStandardMaterialService, multi: true },
     { provide: MATERIAL_SERVICES, useExisting: ElevationMaterialService, multi: true },
+    { provide: MATERIAL_SERVICES, useExisting: TopographicMaterialService, multi: true },
   ]);
 }
